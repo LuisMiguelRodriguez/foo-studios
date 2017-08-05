2fdf7bc26aedfbaf40825b58842280dc00e325fa
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2017-08-05 10:38:08 -0700

    updated call to updated database

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,13 +1,29 @@
 $(document).ready(function() {
 
   getTasks();
-
+  getArtwork();
+  getArtworkInProduction();
 });
 var apiKey = '6LbeBBCgh5azm84Rz5dQejCJv0h4iiRT';
 
+//Shows all collections that are part of foostudiosdemo database
 function getTasks() {
-  $.get('https://api.mlab.com/api/1/databases/foostudios/collections?apiKey=' + apiKey,
+  $.get('https://api.mlab.com/api/1/databases/foostudiosdemo/collections?apiKey=' + apiKey,
 function(data){
   console.log(data);
 });
+}
+
+// Shows all documents in artwork collection from the foostudiosdemo database
+function getArtwork () {
+  $.get('https://api.mlab.com/api/1/databases/foostudiosdemo/collections/artwork?apiKey='+ apiKey, function(data) {
+    console.log(data);
+  });
+}
+
+//Shows art work that is in production
+function getArtworkInProduction () {
+  $.get('https://api.mlab.com/api/1/databases/foostudiosdemo/collections/artwork?q={"production": true}&apiKey=' + apiKey, function ( data) {
+    console.log(data);
+  });
 }
