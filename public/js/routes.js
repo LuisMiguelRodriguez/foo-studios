ed102c8f35bf0c06fdb63dce37796e63be65ee36
Jenmadev <info@maknowsthat.com>
2017-08-09 15:06:46 -0700

    Change the shop and product page to be dynamic

--- public/js/routes.js
+++ public/js/routes.js
@@ -4,6 +4,15 @@ angular.module('Store')
       .when('/about', { templateUrl: 'templates/pages/about/index.html'  })
       .when('/contact', { templateUrl: 'templates/pages/contact/index.html'  })
       .when('/product', { templateUrl: 'templates/pages/product/index.html'  })
-      .when('/shop', { templateUrl: 'templates/pages/shop/index.html'  })
+      .when('/shop', {
+       templateUrl: 'templates/pages/shop/index.html',
+       	controller:'ProductsIndexController',
+       	controllerAs:'indexController'
+         })
       .when('/', { templateUrl: 'templates/pages/home/index.html'  });
+      .when('/shop/:id', {
+       templateUrl: 'templates/pages/shop/index.html',
+        controller:'ProductIndexController',
+        controllerAs:'showController'
+         })
   });
